9c6c2b31b561453f6f72873eae6fc6f22a6afe46
Rhitajyoti Mandal <rhitajyotim411@gmail.com>
2021-11-27 00:34:14 +0530

    Model Testing 2

--- test.js
+++ test.js
@@ -10,8 +10,23 @@
 
 
 const tf = require("@tensorflow/tfjs");
-const tfn = require("@tensorflow/tfjs-node");
-const handler = tfn.io.fileSystem("./tfjs_model/model.json");
-tf.loadLayersModel(handler).then((res)=>{
-	console.log(res)
+//const tfn = require("@tensorflow/tfjs-node");
+//const handler = tfn.io.fileSystem("./tfjs_model/model.json");
+url = "https://raw.githubusercontent.com/SXCSEM6-project/ModelStore/main/model.json"
+
+tf.loadLayersModel(url).then((model)=>{
+	console.log("Success")
+	// input_xs = tf.tensor2d([
+	// 	[1, 0]
+	// ]);
+	// output = model.predict(input_xs);
+	// const outputData = output.dataSync();
+  // op = Number(outputData[0] > 0.5);
+	// console.log(op)
+
+	p=89
+	ip = tf.tensor1d([p/99]);
+	v=model.predict(ip)
+	console.log(v.dataSync())
+
 }).catch((e)=>{console.log(e)})
