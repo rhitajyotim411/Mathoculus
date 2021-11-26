08babb025e659f74c45838dbd1fbd71a48f8e5c4
Rhitajyoti Mandal <rhitajyotim411@gmail.com>
2021-11-26 21:24:54 +0530

    Testing tfjs

---
+++ test.js
@@ -0,0 +1,9 @@
+const tf = require("@tensorflow/tfjs");
+const tfn = require("@tensorflow/tfjs-node");
+// const handler = tfn.io.fileSystem("./path/to/your/model.json");
+//
+// const f= async function(){
+//   const model = await tf.loadLayersModel(handler);
+// }
+//
+// f()
